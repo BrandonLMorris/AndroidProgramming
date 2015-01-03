d685fc7c53a0784071e6d58b8d13febcb274451c
BrandonLMorris <bmorris95@me.com>
2015-01-03 08:04:32 -0600

    first commit

---
+++ GeoQuiz/app/src/main/java/com/example/bmorris/geoquiz/TrueFalse.java
@@ -0,0 +1,31 @@
+package com.example.bmorris.geoquiz;
+
+/**
+ * Created by bmorris on 1/2/15.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        setQuestion(question);
+        setTrueQuestion(trueQuestion);
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
