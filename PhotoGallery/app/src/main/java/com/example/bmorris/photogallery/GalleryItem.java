8c0072494d0d57b2a005850eda60f70c5e2ed142
BrandonLMorris <bmorris95@me.com>
2015-01-07 15:19:18 -0600

    Added app PhotoGallery

---
+++ PhotoGallery/app/src/main/java/com/example/bmorris/photogallery/GalleryItem.java
@@ -0,0 +1,39 @@
+package com.example.bmorris.photogallery;
+
+/**
+ * Created by bmorris on 1/7/15.
+ */
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    public String toString() {
+        return mCaption;
+    }
+
+    //Getters and setters
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+}
